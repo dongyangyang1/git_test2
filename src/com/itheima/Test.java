2d6abc2a0d02712c590ffdefb3741f4b9183dae5
dongyangyang1 <dyydeyou@126.com>
2019-02-23 00:07:52 +0800

    第一次提交代码

---
+++ src/com/itheima/Test.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123455);
+        System.out.println(123455);
+        System.out.println(123455);
+        System.out.println(123455);
+        System.out.println(123455);
+    }
+}
